308ab57fa1ab37f3689dfb4e73c19f4351f5b271
zainmedard <93748161+zainmedard@users.noreply.github.com>
2022-06-24 22:00:16 -0400

    Add files via upload

---
+++ my_batch1.bat
@@ -0,0 +1,13 @@
+@echo off
+echo "start of script"
+#displays message "start of script"
+dir
+#displays contents of directory
+dir/w
+#displays contents in wide view
+echo "end of script"
+#displays message "end of script"
+
+
+
+
